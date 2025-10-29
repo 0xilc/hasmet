e22903fec36a003c025aee64f55b0e99efbe2a37
0xilc <akinaydemir2002@gmail.com>
2025-10-29 12:11:34 +0200

    add material_id to triangle

--- src/geometry/triangle.cpp
+++ src/geometry/triangle.cpp
@@ -2,42 +2,43 @@
 #include <glm/glm.hpp>
 
 Triangle::Triangle(const glm::vec3& p1, const glm::vec3& p2,
-                   const glm::vec3& p3) {
-  indices[0] = p1;
-  indices[1] = p2;
-  indices[2] = p3;
+                   const glm::vec3& p3, int material_id) {
+  indices_[0] = p1;
+  indices_[1] = p2;
+  indices_[2] = p3;
+  material_id_ = material_id;
 }
 
 bool Triangle::intersect(const Ray& ray, HitRecord& rec) const {
-  glm::vec3 c1 = indices[0] - indices[1];
-  glm::vec3 c2 = indices[0] - indices[2];
+  glm::vec3 c1 = indices_[0] - indices_[1];
+  glm::vec3 c2 = indices_[0] - indices_[2];
   glm::vec3 c3 = ray.direction;
   double detA = glm::determinant(glm::mat3(c1, c2, c3));
   if (detA == 0) return false;
 
-  c1 = indices[0] - ray.origin;
+  c1 = indices_[0] - ray.origin;
   double beta = glm::determinant(glm::mat3(c1, c2, c3)) / detA;
 
   c2 = c1;
-  c1 = indices[0] - indices[1];
+  c1 = indices_[0] - indices_[1];
   double gamma = glm::determinant(glm::mat3(c1, c2, c3)) / detA;
 
   c3 = c2;
-  c2 = indices[0] - indices[2];
+  c2 = indices_[0] - indices_[2];
   double t = glm::determinant(glm::mat3(c1, c2, c3)) / detA;
 
   if (t < ray.tmin + 0.0001 || 0.0001 + t > ray.tmax) return false;
 
   if (beta + gamma <= 1 && beta + 0.00001 >= 0 && gamma + 0.00001 >= 0) {
-    glm::vec3 vec1 = indices[1] - indices[0];
-    glm::vec3 vec2 = indices[2] - indices[0];
+    glm::vec3 vec1 = indices_[1] - indices_[0];
+    glm::vec3 vec2 = indices_[2] - indices_[0];
     vec1 = glm::cross(vec1, vec2);
     vec1 = glm::normalize(vec1);
 
     rec.t = t;
     rec.normal = vec1;
     rec.p = ray.origin + ray.direction * static_cast<float>(t);
-    // TODO: Implement this after material manager. rec.mat_ptr = mat_ptr;
+    rec.material_id = material_id_;
     return true;
   }
   return false;
--- src/geometry/triangle.h
+++ src/geometry/triangle.h
@@ -5,9 +5,10 @@
 
 class Triangle : Shape {
  public:
-  Triangle(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3);
+  Triangle(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3, int material_id);
   bool intersect(const Ray& ray, HitRecord& rec) const override;
 
  private:
-  glm::vec3 indices[3];
+  glm::vec3 indices_[3];
+  int material_id_;
 };
