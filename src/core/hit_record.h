88512099d031e723f319de1420a36e14cf978a0e
0xilc <akinaydemir2002@gmail.com>
2026-01-18 19:24:52 +0300

    Implement a basic path tracer

--- src/core/hit_record.h
+++ src/core/hit_record.h
@@ -3,6 +3,7 @@
 #include <glm/glm.hpp>
 #include "ray.h"
 #include "core/types.h"
+#include <optional>
 
 namespace hasmet {
 struct HitRecord {
@@ -14,6 +15,7 @@ struct HitRecord {
   int material_id;
   Vec2 uv{0.0f, 0.0f};
   const std::vector<int>* texture_ids = nullptr;
+  std::optional<Color> radiance;
   
   inline void set_face_normal(const Ray& r, const Vec3& outward_normal) {
     front_face = glm::dot(r.direction, outward_normal);
