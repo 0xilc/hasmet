42dd95f0c5f9e3bc72a8d9004c38e61c0082d11a
0xilc <akinaydemir2002@gmail.com>
2026-01-20 16:16:33 +0300

    Fix whitted integrator dark image bug

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char* argv[]) {
 
   try {
     LOG_INFO("Reading scene...");
-    std::string scene_path = "/home/ilc/Desktop/hw1/directLighting/inputs/cornellbox_jaroslav_glossy_area_sphere.json";
+    std::string scene_path = "/home/ilc/Desktop/hw1/inputs/cornellbox.json";
     Scene scene = Parser::ParserAdapter::read_scene(scene_path);
     for (const std::unique_ptr<Camera>& camera : scene.cameras_) {
       Film film(camera->film_width_, camera->film_height_, camera->image_name_);
